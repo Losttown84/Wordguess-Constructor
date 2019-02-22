e2d232bc74e1822af9e45ca78eb1fc63473dea9d
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-02-22 13:21:45 -0600

    added in functions for choosing a word from the array and one for allowing users to guess a letter(correctly incomplete) for index

--- index.js
+++ index.js
@@ -3,3 +3,37 @@ var inquirer = require ("inquirer");
 var 
 
 creekList = ["DAVID ROSE", "ALEXIS ROSE", "MOIRA ROSE", "JOHNNY ROSE", "TED MULLENS", "JOCELYN SCHITT", "STEVIE BUDD", "ROLAND SCHITT", "TWYLA SANDS", "PATRICK BREWER", "RONNIE LEE"];
+var selectWord = 0;
+var chooseWord = "";
+var gameWord = "";
+var counter = 0;
+
+function startGame(){
+  if (creekList.length<2) {
+    creekList = ["DAVID ROSE", "ALEXIS ROSE", "MOIRA ROSE", "JOHNNY ROSE", "TED MULLENS", "JOCELYN SCHITT", "STEVIE BUDD", "ROLAND SCHITT", "TWYLA SANDS", "PATRICK BREWER", "RONNIE LEE"];
+  }
+  select = Math.floor(Math.random() * creekList.length);
+  chooseWord = creekList[select];
+  gameWord = new Word(chooseWord);
+  gameWord.makeWord();
+  if (select > -1) {
+    creekList.splice(select, 1);
+  }
+  console.log("\nYou have 9 chances to guess the Schitt's Creek character")
+  promptUser();
+}
+
+function promptUser() {
+  if (counter < 9) {
+      console.log(gameWord.showWord());
+      inquirer.prompt ([
+        {
+          type: "input",
+          name: "letter",
+          message: "\nPlease pick a letter and then enter"
+        }
+      ]).then(function(data) {
+        checkAnswer(data);
+      });
+  }
+}
