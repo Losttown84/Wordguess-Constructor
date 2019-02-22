b5dd9d6113b47e232b30306550e2ab412f293a43
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-02-22 11:24:34 -0600

    added in word list for guessing

---
+++ index.js
@@ -0,0 +1,5 @@
+var Word = require ("./word.js");
+var inquirer = require ("inquirer");
+var 
+
+creekList = ["DAVID ROSE", "ALEXIS ROSE", "MOIRA ROSE", "JOHNNY ROSE", "TED MULLENS", "JOCELYN SCHITT", "STEVIE BUDD", "ROLAND SCHITT", "TWYLA SANDS", "PATRICK BREWER", "RONNIE LEE"];
