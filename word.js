d79de68e41b0fb1749f903c0d375733ee3105cf5
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-02-24 10:55:29 -0600

    word js file functions to update words from the letters chosen by user

--- word.js
+++ word.js
@@ -1,4 +1,17 @@
-function Word(word)
+var Let = require("./letter.js");
+
+function Word(rightName) {
+  this.rightName = rightName;
+  this.letters = [];
+
+
+  this.createLetters = function() {
+    var wordArray = this.rightName.split("");
+    for (var i=0; i < wordArray.length; i++) {
+      var letter = new
+    }
+  }
+}
 
 
 
