11e31dcda812ae65d6ee3210b23e9cdc90b1847d
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-02-24 10:17:12 -0600

    letter js file written out with letter guesses and checks

--- letter.js
+++ letter.js
@@ -1,7 +1,23 @@
 function Let(letter) {
+  this.letter = letter.toUpper();
   this.guessedCorrect = false;
-  this.
-}
+
+  this.characterReturn = function(){
+    if (this.guessedCorrect) {
+      return this.letter;
+    } else {
+      return "_";
+    };
+  };
+
+  this.check = function(guess) {
+    if (this.letter.toLower() == guess.toLower()) {
+      this.guessedCorrect = true;
+    } else {
+
+    };
+  };
+};
 
 
 module.exports = Let;
